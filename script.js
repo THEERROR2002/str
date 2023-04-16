2af20dcc8e070fdb629b824d5daeb57dddec6377
THEERROR2002 <72090888+THEERROR2002@users.noreply.github.com>
2023-04-16 20:53:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function play() {
-    $('#ziak').trigger('play');
-    var vid = document.querySelector('video');
-    vid.style.visibility = 'visible';
-    vid.volume = "0.1";
 
-    var bio = document.getElementById('bio');
-    bio.style.visibility = 'visible';
- }
